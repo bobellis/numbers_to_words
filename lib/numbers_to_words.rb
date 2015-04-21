a1e72b45d0ec9a645c2417ba7e919b3d01cbb8da
bobellis & Jesse James <student@epicodus.com>
2015-04-21 15:30:21 -0700

    Added additional tests for non-multiples of 10, teens, and multiples of 100

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -4,21 +4,31 @@ class Fixnum
     tens_place = ''
     ones = { 0 => "", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
     multiples_of_ten = {1 => "ten",2 => "twenty",3 => "thirty",4 => "forty",5 => "fifty",6 => "sixty",7 => "seventy",8 => "eighty",9 => "ninety"}
+    teens = {0 => "ten", 1 => "eleven", 2 => "twelve", 3 => "thirteen",4 => "fourteen", 5 => "fifteen", 6 => "sixteen", 7 => "seventeen", 8 => "eighteen", 9 => "nineteen"}
     converted_numbers = self.to_s().split('')
     converted_numbers_length = converted_numbers.length()
     # converted_numbers.each() do |number|
-      if converted_numbers_length == 1
-        temp_at = (converted_numbers_length -= 1)
-        array_index = temp_at.to_i()
-        array_loc = converted_numbers.at(array_index)
-        ones_place = ones.fetch(array_loc.to_i())
-      elsif converted_numbers_length == 2
-        temp_at = (converted_numbers_length -= 2)
-        array_index = temp_at.to_i()
-        array_loc = converted_numbers.at(array_index)
-        tens_place = multiples_of_ten.fetch(array_loc.to_i())
-      end
-    # end
+    if converted_numbers_length == 1
+      array_index = (converted_numbers_length - 1)
+      array_loc = converted_numbers.at(array_index).to_i()
+      ones_place = ones.fetch(array_loc)
+      word_number = ones_place
+    elsif converted_numbers_length == 2 && converted_numbers.at(1) == "0"
+      array_index = (converted_numbers_length - 2)
+      array_loc = converted_numbers.at(array_index).to_i()
+      tens_place = multiples_of_ten.fetch(array_loc)
+      word_number = tens_place
+    elsif converted_numbers_length == 2 && converted_numbers.at(0) != "1"
+      tens_array_loc = converted_numbers.at(0).to_i()
+      tens_place = multiples_of_ten.fetch(tens_array_loc)
+      ones_array_loc = converted_numbers.at(1).to_i()
+      ones_place = ones.fetch(ones_array_loc)
       word_number = tens_place + ' ' + ones_place
+    elsif converted_numbers_length == 2
+      teens_array_loc = converted_numbers.at(1).to_i()
+      teens_place = teens.fetch(teens_array_loc)
+      word_number = teens_place
+    end
+    word_number
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -5,7 +5,20 @@ describe('String#numbers_to_words') do
   it('take a single digit Fixnum and return the string value') do
     expect(4.numbers_to_words()).to(eq("four"))
   end
+
   it("takes a two digit number that's a multiple of 10 and returns the string value") do
     expect(20.numbers_to_words()).to(eq("twenty"))
+  end
+
+  it('takes a two digit number that is not a multiple of 10 and returns the string value') do
+    expect(23.numbers_to_words()).to(eq("twenty three"))
+  end
+
+  it('takes a two digit number that is between 10 and 19 and returns the string value') do
+    expect(18.numbers_to_words()).to(eq("eighteen"))
+  end
+
+  it('takes three digit number that is a multiple of 100 and returns the string value') do
+    expect(200.numbers_to_words()).to(eq("two hundred"))
   end
 end
