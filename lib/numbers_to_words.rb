7d70e49f5b1e48505df535c5018568df8fd79a52
bobellis & Jesse James <student@epicodus.com>
2015-04-21 14:42:06 -0700

    Wrote 2nd test and passed.  Added multiples of 10 hash to handle things like twenty and thirty

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,6 +1,24 @@
 class Fixnum
   define_method(:numbers_to_words) do
-    ones = { 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
-    word_number = ones.fetch(self)
+    ones_place = ''
+    tens_place = ''
+    ones = { 0 => "", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
+    multiples_of_ten = {1 => "ten",2 => "twenty",3 => "thirty",4 => "forty",5 => "fifty",6 => "sixty",7 => "seventy",8 => "eighty",9 => "ninety"}
+    converted_numbers = self.to_s().split('')
+    converted_numbers_length = converted_numbers.length()
+    # converted_numbers.each() do |number|
+      if converted_numbers_length == 1
+        temp_at = (converted_numbers_length -= 1)
+        array_index = temp_at.to_i()
+        array_loc = converted_numbers.at(array_index)
+        ones_place = ones.fetch(array_loc.to_i())
+      elsif converted_numbers_length == 2
+        temp_at = (converted_numbers_length -= 2)
+        array_index = temp_at.to_i()
+        array_loc = converted_numbers.at(array_index)
+        tens_place = multiples_of_ten.fetch(array_loc.to_i())
+      end
+    # end
+      word_number = tens_place + ' ' + ones_place
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -5,4 +5,7 @@ describe('String#numbers_to_words') do
   it('take a single digit Fixnum and return the string value') do
     expect(4.numbers_to_words()).to(eq("four"))
   end
+  it("takes a two digit number that's a multiple of 10 and returns the string value") do
+    expect(20.numbers_to_words()).to(eq("twenty"))
+  end
 end
